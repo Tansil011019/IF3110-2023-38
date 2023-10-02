7a1def01b9451ed232d2233e4284f98c7ab5a5df
Tansil011019 <13521054@std.stei.itb.ac.id>
2023-10-02 14:36:20 +0700

    feat: add global var

--- src/app/index.php
+++ src/app/index.php
@@ -1,3 +1,30 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type='text/css' href="styles/global.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+    <link rel="shortcut icon" href="public/favicon.svg" type="image/svg+xml">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <?php
+            require_once 'components/BookinInput.php';
 
-echo 'hello world';
+            $props = [
+                'name' => 'testing-only',
+                'type' => 'email',
+                'placeHolder' => 'Password'
+            ];
+
+            $inputTest = BookinInput($props);
+
+            echo $inputTest;    
+        ?>
+    </div>
+</body>
+</html>
