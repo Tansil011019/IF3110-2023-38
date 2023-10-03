f7485eccd70b4f0caf95bd91329f175ad8cdb8c6
Tansil011019 <13521054@std.stei.itb.ac.id>
2023-10-03 13:38:13 +0700

    feat: add current router and initial page

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -2,8 +2,9 @@
 
 class Home extends Controller {
     public function index () {
-        $data['judul'] = 'Home Page';
-        $this->view('templates/header', $data);
+        $headerData['title'] = 'Home Page';
+        $headerData['currentRoute'] = '/home';
+        $this->view('templates/header', $headerData);
         $this->view('home/index');
         $this->view('templates/footer');
     }
