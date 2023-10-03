899b1eb7b87ef05bb25de040460efc7b227a4cd6
Tansil011019 <13521054@std.stei.itb.ac.id>
2023-10-03 18:58:09 +0700

    feat: add css factory and discoverMovies ui

--- app/views/components/home/discoverMovies.php
+++ app/views/components/home/discoverMovies.php
@@ -1,5 +1,34 @@
 <div class="discover-movie-container">
-    <?php
-        require_once 'app/views/components/home/discoverMoviesData.php'
-    ?>
+    <div class="discover-movie-header">
+        <div class="discover-movie-title">
+            Discover Movies
+        </div>
+        <div class="discover-movie-search-bar">
+            <?php 
+                require_once 'app/views/libs/function/BookinInput.php';
+
+                $inputProps = [
+                    'name' => 'discover-movies-search-bar',
+                    'placeHolder' => "Search Movie",
+                ];
+
+                $inputField = BookinInput($inputProps);
+                
+                echo $inputField;
+            ?>
+        </div>
+    </div>
+    <div class="discover-movie-body">
+        <div class="discover-movie-filter">
+            <div class="discover-movie-filter-label">
+                Sort By:
+            </div>
+            <div class="discover-movie-filter-dropdown">
+                Here Will Be The Dropdown!
+            </div>
+        </div>
+        <?php
+            require_once 'app/views/components/home/discoverMoviesData.php'
+        ?>
+    </div>
 </div>
---
+++ app/views/ui/bookinPagination.php
@@ -0,0 +1,3 @@
+<div class="discover-movies-list-pagination">
+    There will be pagination here!
+</div>
