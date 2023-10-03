9a9805f910050de801555b7d5eb3f4daf80a2070
Tansil011019 <13521054@std.stei.itb.ac.id>
2023-10-03 21:59:33 +0700

    refactor: change item card to link

--- app/views/ui/bookinCard.php
+++ app/views/ui/bookinCard.php
@@ -1,13 +1,17 @@
-<div class="card-container">
-    <div class="card-image">
-        <img src="<?= $datum['movieImage']; ?>" alt="card image">
-    </div>
-    <div class="card-description">
-        <div class="card-genre">
-            <?= $datum['movieGenre']; ?>
+<div class="card-outer-container">
+    <a href="#">
+        <div class="card-container">
+                <div class="card-image">
+                    <img src="<?= $datum['movieImage']; ?>" alt="card image">
+                </div>
+                <div class="card-description">
+                    <div class="card-genre">
+                        <?= $datum['movieGenre']; ?>
+                    </div>
+                    <div class="card-movie-title">
+                        <?= $datum['movieTitle']; ?>
+                    </div>
+                </div>
         </div>
-        <div class="card-movie-title">
-            <?= $datum['movieTitle']; ?>
-        </div>
-    </div>
+    </a>
 </div>
