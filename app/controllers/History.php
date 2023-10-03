ca45239e46ede88c6f408703e6bb2e3f848ed046
Tansil011019 <13521054@std.stei.itb.ac.id>
2023-10-03 17:07:09 +0700

    feat: add external css seperately for all pages

--- app/controllers/History.php
+++ app/controllers/History.php
@@ -4,6 +4,8 @@ class History extends Controller {
     public function index() {
         $headerData['title'] = 'History Page';
         $headerData['currentRoute'] = '/history';
+        $headerData['style'] = '/public/css/history.css';
+
         $this->view('templates/header', $headerData);
         $this->view('history/index');
         $this->view('templates/footer');
