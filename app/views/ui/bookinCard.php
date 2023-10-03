96e365adf2ed5cab69d5e116f65720f297dc013a
Tansil011019 <13521054@std.stei.itb.ac.id>
2023-10-03 17:00:53 +0700

    feat: add card for movie

---
+++ app/views/ui/bookinCard.php
@@ -0,0 +1,13 @@
+<div class="card-container">
+    <div class="card-image">
+        <img src="<?= $datum['movieImage']; ?>" alt="card image">
+    </div>
+    <div class="card-description">
+        <div class="card-genre">
+            <?= $datum['movieGenre']; ?>
+        </div>
+        <div class="card-movie-title">
+            <?= $datum['movieTitle']; ?>
+        </div>
+    </div>
+</div>
