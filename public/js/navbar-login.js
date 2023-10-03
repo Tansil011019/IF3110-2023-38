743f075fd05b12e5ccc9a0c86263c20e5e7f82b4
Tansil011019 <13521054@std.stei.itb.ac.id>
2023-10-03 15:26:44 +0700

    fixed: will remain login if refresh

--- public/js/navbar-login.js
+++ public/js/navbar-login.js
@@ -1,10 +1,18 @@
-function toogleItems() {
+function login() {
+    localStorage.setItem('isLoggedIn', 'true');
+
+    handleLoginState();
+}
+
+function handleLoginState() {
+    const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
+
     const scheduleItem = document.querySelector('.nav-link-schedule');
     const historyItem = document.querySelector('.nav-link-history');
     const loginButton = document.querySelector('.login-btn');
-    const profileDropdown = document.querySelector('.profile-dropdown')
-    
-    if (scheduleItem.style.display === 'none' || scheduleItem.style.display === '') {
+    const profileDropdown = document.querySelector('.profile-dropdown');
+
+    if (isLoggedIn) {
         scheduleItem.style.display = 'block';
         historyItem.style.display = 'block';
         profileDropdown.style.display = 'block';
@@ -17,7 +25,12 @@ function toogleItems() {
     }
 }
 
-document.querySelector('.sign-out-link').addEventListener('click', function(e) {
+document.addEventListener('DOMContentLoaded', function () {
+    handleLoginState();
+});
+
+document.querySelector('.sign-out-link').addEventListener('click', function (e) {
     e.preventDefault();
-    toogleItems();
-})
+    localStorage.setItem('isLoggedIn', 'false');
+    handleLoginState();
+});
