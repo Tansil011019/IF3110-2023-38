e20717534986e0c0d297edffdaffc8e0a97cc666
Tansil011019 <13521054@std.stei.itb.ac.id>
2023-10-04 17:12:50 +0700

    feat: add login page

--- app/views/auth/login/index.php
+++ app/views/auth/login/index.php
@@ -1,37 +1,47 @@
 <div class="login-page-container">
-    <div class="login-title-label">
-        Welcome Back
-    </div>
-    <div class="login-form">
-        <div class="login-form-email-address-container">
-            <?php
-            require_once 'app/views/libs/function/BookinInput.php';
-
-            $inputProps = [
-                'name' => 'login-form-email-address',
-                'placeHolder' => "Email Address",
-                'type' => "email",
-            ];
-
-            $inputField = BookinInput($inputProps);
-
-            echo $inputField;
-            ?>
-        </div>
-        <div class="login-form-password-container">
-            <?php
-            require_once 'app/views/libs/function/BookinInput.php';
-
-            $inputProps = [
-                'name' => 'login-form-password',
-                'placeHolder' => "password",
-                'type' => 'password',
-            ];
-
-            $inputField = BookinInput($inputProps);
-
-            echo $inputField;
-            ?>
+    <div class="login-page-background">
+        <div class="login-form">   
+            <div class="login-form-upper">
+                <div class="login-title-label">
+                    Welcome Back!
+                </div>
+                <div class="login-form-component">
+                    <div class="login-form-email-address-container">
+                        <?php
+                        require_once 'app/views/libs/function/BookinInput.php';
+        
+                        $inputProps = [ 
+                            'name' => 'login-form-email-address',
+                            'placeHolder' => "Email Address",
+                            'type' => "email",
+                        ];
+                        $inputField = BookinInput($inputProps);
+        
+                        echo $inputField;
+                        ?>
+                    </div>
+                    <div class="login-form-password-container">
+                        <?php
+                        require_once 'app/views/libs/function/BookinInput.php';
+        
+                        $inputProps = [
+                            'name' => 'login-form-password',
+                            'placeHolder' => "Password",
+                            'type' => 'password',
+                        ];
+        
+                        $inputField = BookinInput($inputProps);
+        
+                        echo $inputField;
+                        ?>
+                    </div>
+                </div>
+            </div>
+            <div class="login-form-bottom">
+                <button class="log-in-button">
+                    Log In
+                </button>
+            </div>
         </div>
     </div>
 </div>
