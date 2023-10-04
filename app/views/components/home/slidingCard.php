a6622f2e70d979139c69f992d61d1b89269ccfd3
Tansil011019 <13521054@std.stei.itb.ac.id>
2023-10-04 07:34:27 +0700

    feat: add init sliding card

--- app/views/components/home/slidingCard.php
+++ app/views/components/home/slidingCard.php
@@ -1,3 +1,56 @@
 <div class="sliding-card-home-container">
-    <img src="" alt="">
+    <div class="sliding-card-image">
+        <img src="/public/images/bookin-default-carousel-img.svg" data-desc="Image1" alt="default carousel image" class="active">
+        <img src="/public/images/bookin-default-carousel-img.svg" data-desc="Image2" alt="default carousel image">
+        <img src="/public/images/bookin-default-carousel-img.svg" data-desc="Image3" alt="default carousel image">
+        <img src="/public/images/bookin-default-carousel-img.svg" data-desc="Image4" alt="default carousel image">
+        <img src="/public/images/bookin-default-carousel-img.svg" data-desc="Image5" alt="default carousel image">
+    </div>
+    <div class="sliding-card-home-nav">
+        <div class="sliding-card-home-nav-dots"></div>
+        <div class="sliding-card-home-nav-desc">
+            <div class="sliding-card-home-nav-desc-today-movie-schedule">
+                <div class="sliding-card-home-nav-desc-today-movie-schedule-label">
+                    Today:
+                </div>
+                <!-- dummy element -->
+                <div class="sliding-card-home-nav-desc-today-movie-schedule-times">
+                    <div class="sliding-card-home-nav-desc-today-movie-schedule-time">
+                        11:45
+                    </div>
+                    <div class="sliding-card-home-nav-desc-today-movie-schedule-time">
+                        13:45
+                    </div>
+                    <div class="sliding-card-home-nav-desc-today-movie-schedule-time">
+                        15:45
+                    </div>
+                </div>
+            </div>
+            <div class="additional-desc-and-tools">
+                <!-- dummy element -->
+                <div class="sliding-card-home-nav-desc-movie-duration">
+                    148 min
+                </div>
+                <!-- dummy element -->
+                <div class="sliding-card-home-nav-desc-movie-age-restriction">
+                    12+
+                </div>
+                <div class="sliding-card-home-nav-desc-movie-watch-trailer">
+                    <button class="sliding-card-home-nav-desc-movie-watch-movie-trailer-button">
+                        <div class="button-text-description-watch-trailer">
+                            Watch Trailer 
+                        </div>
+                        <img src="/public/icons/bookin-play-video-button-ic.svg" alt="play button icon">
+                    </button>
+                </div>
+                <div class="sliding-card-home-nav-desc-movie-next-sliding-card">
+                    <button class="sliding-card-home-nav-desc-movie-next-sliding-card-button">
+                        <img src="/public/icons/bookin-arrow-right-ic.svg" alt="arrow right icon">
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+
+<script src="/public/js/slide-card-carousel.js"></script>
