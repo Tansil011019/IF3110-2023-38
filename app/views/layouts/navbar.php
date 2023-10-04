0dba45a0f44f89301f71a381ab60ff60ff10032c
Tansil011019 <13521054@std.stei.itb.ac.id>
2023-10-04 18:42:04 +0700

    fix: navbar logo

--- app/views/layouts/navbar.php
+++ app/views/layouts/navbar.php
@@ -1,17 +1,21 @@
 <nav class="navbar">
     <a href="#" class="logo">
-        <img class="navbar-logo" src="/public/images/bookin-logo.svg" alt="bookin logo">
+        <h1 class="logo">
+            <div class="blur-box"></div>
+            Book.in
+        </h1>
     </a>
     <div class="navbar-menu">
-        <ul class = "nav__link">
-            <li class = "nav-link-home <?= $data['currentRoute'] === "/home" ? "active" : "" ?>"><a href="/home">Home</a></li>
-            <li class = "nav-link-schedule <?= $data['currentRoute'] === "/schedule" ? "active" : "" ?>"><a href="/schedule">Schedule</a></li>
-            <li class = "nav-link-history <?= $data['currentRoute'] === "/history" ? "active" : "" ?>"><a href="/history">History</a></li>
+        <ul class="nav__link">
+            <li class="nav-link-home <?= $data['currentRoute'] === "/home" ? "active" : "" ?>"><a href="/home">Home</a></li>
+            <li class="nav-link-schedule <?= $data['currentRoute'] === "/schedule" ? "active" : "" ?>"><a href="/schedule">Schedule</a></li>
+            <li class="nav-link-history <?= $data['currentRoute'] === "/history" ? "active" : "" ?>"><a href="/history">History</a></li>
+            <li class="nav-link-login <?= $data['currentRoute'] === "/login" ? "active" : "" ?>"><a href="/login">Login</a></li>
         </ul>
-        <button class="login-btn" onclick="login()">Login</button>
+        <button class="register-btn" onclick="register()">Register</button>
         <div class="profile-dropdown">
             <button class="dropbtn">
-                <img src="/public/icons/bookin-profile-ic.svg" alt="bookin profile icon">   
+                <img src="/public/icons/bookin-profile-ic.svg" alt="bookin profile icon">
             </button>
             <ul class = "profile__link">
                 <li class = "profile-link-profile">
@@ -22,7 +26,7 @@
                         </div>
                     </a>
                 </li>
-                <li class = "profile-link-signout">
+                <li class="profile-link-signout">
                     <a href="#" class="sign-out-link">
                         <div class="sign-out-menu-content content">
                             <img src="/public/icons/bookin-sign-out-ic.svg" alt="bookin sign out icon">
@@ -35,4 +39,4 @@
     </div>
 </nav>
 
-<script src="/public/js/navbar-login.js"></script>
+<script src="/public/js/navbar-register.js"></script>
