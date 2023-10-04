e15b398d401110e5781fb61885fdcdfb1abcafa7
Tansil011019 <13521054@std.stei.itb.ac.id>
2023-10-04 17:12:50 +0700

    feat: add login page

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,6 +1,12 @@
-        <footer>
-            
-        </footer>
+        <?php
+        $currentUrl = $_SERVER['REQUEST_URI'];
+
+        $excludedUrls = ['/login', '/register'];
+
+        if (!in_array($currentUrl, $excludedUrls)) {
+            echo '<footer></footer>';
+        }
+        ?>
         <script src="/public/js/password-input-eye-button.js"></script>
     </body>
 </html>
