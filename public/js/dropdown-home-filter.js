60b8de4169d8073d623093174f5792a429598707
Tansil011019 <13521054@std.stei.itb.ac.id>
2023-10-06 20:05:21 +0700

    feat: add status filter

--- public/js/dropdown-home-filter.js
+++ public/js/dropdown-home-filter.js
@@ -1,18 +1,19 @@
-var keywordGenre = document.querySelector(".genres");
+var keywordGenre = document.querySelector(".genres-film");
+var keywordStatus = document .querySelector(".status-film")
 var containerCard = document.querySelector(".container-bookin-data");
 
-keywordGenre.addEventListener("change", function () {
-    console.log(window.location.href)
-    console.log(keywordGenre.value)
-  var xhr = new XMLHttpRequest();
-
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      containerCard.innerHTML = xhr.responseText;
-    }
-  };
-
-  xhr.open("GET", "/movie?genre=" + keywordGenre.value, true);
-  xhr.send();
-
-});
+[keywordGenre, keywordStatus].forEach(function(keywordElmt) {
+  keywordElmt.addEventListener("change", function () {
+    var xhr = new XMLHttpRequest();
+  
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        containerCard.innerHTML = xhr.responseText;
+      }
+    };
+  
+    xhr.open("GET", "/movie?genre=" + keywordGenre.value + "&status=" + keywordStatus.value, true);
+    xhr.send();
+  
+  });
+})
