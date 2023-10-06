66a4146d00626c8efff1efa58ae7a3bd51f6b5bb
Agilham <13521118@std.stei.itb.ac.id>
2023-10-06 19:22:33 +0700

    feat: add movie list page (fe)

---
+++ app/views/components/movieList/adminMovie.php
@@ -0,0 +1,12 @@
+<div class="admin-movie-container">
+    <div class="admin-movie-header">
+        <div class="admin-movie-title">
+            Movie List
+        </div>
+    </div>
+    <div class="admin-movie-body">
+        <?php
+            require_once 'app/views/components/movieList/adminMovieData.php'
+        ?>
+    </div>
+</div>
