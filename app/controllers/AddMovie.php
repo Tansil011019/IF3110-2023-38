c17e7e5fc4052c5efb6157b455d82e23beedb52a
Agilham <13521118@std.stei.itb.ac.id>
2023-10-06 22:53:00 +0700

    feat: be for add movie

--- app/controllers/AddMovie.php
+++ app/controllers/AddMovie.php
@@ -4,8 +4,40 @@ class AddMovie extends Controller {
     public function index() {
         $data['header'] = $this->model('AddMovieModel')->getHeader();
 
-        $this->view('templates/header', $data);
-        $this->view('addMovie/index');
-        $this->view('templates/footer');
+        try {
+            switch ($_SERVER['REQUEST_METHOD']) {
+                case 'GET':
+                    // Load addMovie.php
+                    $this->view('templates/header', $data);
+                    $this->view('addMovie/index');
+                    $this->view('templates/footer');
+                    exit;
+
+                    break;
+                case 'POST':
+                    /* Lakukan validasi */
+                    // Form tidak lengkap
+                    if (!$_POST['title'] || !$_POST['artist'] || !$_POST['date'] || !$_POST['genre']) {
+                        throw new LoggedException('Bad Request', 400);
+                    }
+                    // File tidak diisi
+                    if ($_FILES['cover']['error'] === 4) {
+                        throw new LoggedException('Bad Request', 400);
+                    }
+
+                    $albumModel = $this->model('AlbumModel');
+                    $albumID = $albumModel->createAlbum($_POST['title'], $_POST['genre'], $_POST['description'], $_POST['age_restriction'], $_POST['duration'], $_POST['starting_date'], $_POST['end_date'], $_POST['trailer_url_youtube'], $_POST['poster_url'], $_POST['trailer_url'], $_POST['thumbnail_url']);
+
+                    header("Location: /public/album/detail/$albumID", true, 301);
+                    exit;
+
+                    break;
+                default:
+                    throw new LoggedException('Method Not Allowed', 405);
+            }
+        } catch (Exception $e) {
+            http_response_code($e->getCode());
+            exit;
+        }
     }
 }
--- app/models/AddMovieModel.php
+++ app/models/AddMovieModel.php
@@ -1,14 +1,41 @@
 <?php
 class AddMovieModel
 {
+    private $db;
+
     private $modelInfo = [
         "title" => "Add Movie",
         "currentRoute" => "/addMovie",
         "style" => "public/css/addMovie.css",
     ];
 
+    public function __construct()
+    {
+        $this->db = new Database;
+    }
+
     public function getHeader()
     {
         return $this->modelInfo;
+    }
+
+    public function createMovie($title, $genre, $description, $age_restriction, $duration, $starting_date, $end_date, $trailer_url_youtube, $poster_url, $trailer_url, $thumbnail_url)
+    {
+        $query = 'INSERT INTO film (title, genre, description, age_restriction, duration, starting_date, end_date, trailer_url_youtube, poster_url, trailer_url, thumbnail_url) VALUES (title, genre, description, age_restriction, duration, starting_date, end_date, trailer_url_youtube, poster_url, trailer_url, thumbnail_url)';
+
+        $this->db->query($query);
+        $this->db->bind('title', $title);
+        $this->db->bind('genre', $genre);
+        $this->db->bind('description', $description);
+        $this->db->bind('age_restriction', $age_restriction);
+        $this->db->bind('duration', $duration);
+        $this->db->bind('starting_date', $starting_date);
+        $this->db->bind('end_date', $end_date);
+        $this->db->bind('trailer_url_youtube', $trailer_url_youtube);
+        $this->db->bind('poster_url', $poster_url);
+        $this->db->bind('trailer_url', $trailer_url);
+        $this->db->bind('thumbnail_url', $thumbnail_url);
+
+        $this->db->execute();
     }
 }
