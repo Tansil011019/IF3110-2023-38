42046c32eb154aa4534ad0b3f0c26e89d7e07873
Agilham <13521118@std.stei.itb.ac.id>
2023-10-06 19:37:45 +0700

    feat: add sort button

--- app/views/components/movieList/adminMovie.php
+++ app/views/components/movieList/adminMovie.php
@@ -3,6 +3,14 @@
         <div class="admin-movie-title">
             Movie List
         </div>
+        <div class="admin-movie-sorting-container">
+            <div class="sort-attribute-dropdown">
+                <button>Sort By:</button>
+            </div>
+            <div class="sort-order-dropdown">
+                <button>Sort Order:</button>
+            </div>
+        </div>
     </div>
     <div class="admin-movie-body">
         <?php
