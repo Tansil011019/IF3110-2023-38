f98c7d6a00f1f228751ef722b47c19652331b6a4
Agilham <13521118@std.stei.itb.ac.id>
2023-10-06 17:59:34 +0700

    refactor: change class name

--- app/views/components/history/transactionHistory.php
+++ app/views/components/history/transactionHistory.php
@@ -1,10 +1,10 @@
-<div class="discover-history-container">
-    <div class="discover-history-header">
-        <div class="discover-history-title">
+<div class="transaction-history-container">
+    <div class="transaction-history-header">
+        <div class="transaction-history-title">
             My Order History
         </div>
     </div>
-    <div class="discover-history-body">
+    <div class="transaction-history-body">
         <?php
             require_once 'app/views/components/history/transactionHistoryData.php'
         ?>
