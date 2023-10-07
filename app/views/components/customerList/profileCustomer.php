40ad3e33d539e1eda8d868ed686ed28dd250d178
muhhanan <92153837+tarsn@users.noreply.github.com>
2023-10-07 21:30:12 +0700

    feat : add modal for remove-customer

--- app/views/components/customerList/profileCustomer.php
+++ app/views/components/customerList/profileCustomer.php
@@ -2,7 +2,9 @@
 
 function profileCustomer($i, $name, $email)
 {
+    $modalId = "myModal-" . $i; // Buat ID modal yang unik
     $html = <<< "EOT"
+        <script src="public/js/costomer-list.js"></script>
         <div class="profile-customer-container">
             <div class="left-side">
                 <div class="list-number">
@@ -21,9 +23,20 @@ function profileCustomer($i, $name, $email)
                 </div>
             </div>
             <div class="right-side">
-                <button id="kick-customer">
+                <button class="kick-customer" data-customer-id="$i">
                     <img src=public/images/bookin-customerlist-remove.svg>
                 </button>
+            </div>
+        </div>
+        <div id="$modalId" class="modal-confirmation">
+            <div class="modal-content">
+                <!-- <span class="close">&times;</span> -->
+                <div class="confirmation-label">Are you sure want to remove this customer?</div>
+                <div class="confirmation-button">
+                    <button id="confirm-kick" data-customer-id="$i">Yes</button>
+                    <button id="cancel-kick" data-customer-id="$i">No</button>
+                    <script src="public/js/costomer-list.js"></script>
+                </div>
             </div>
         </div>
     EOT;
