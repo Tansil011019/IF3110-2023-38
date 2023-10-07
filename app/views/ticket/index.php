ffcc7b5e41a3f78c34f3fa1d98dd044f1da2575f
muhhanan <92153837+tarsn@users.noreply.github.com>
2023-10-08 04:01:52 +0700

    feat : fix css and component for purchase ticket

--- app/views/ticket/index.php
+++ app/views/ticket/index.php
@@ -32,26 +32,30 @@
                         <div>129 Minutes</div>
                     </div>
                 </div>
-                <div>
+                <div class="ticket-info-right-desc-label">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </div>
                 <div class="ticket-info-right-datetime-container">
                     <div class="ticket-info-right-date">
-                        <div>Select Date</div>
+                        <div class="label-datetime">Select Date</div>
                         <div class="line-datetime-container">
                             <img src="/public/icons/bookin-line-solid-1.svg" alt="Solid Line 1" />
                         </div>
+
+                        <!-- ganti -->
                         <div class="button-text-border">
                             <button>Thursday, 4 July</Button>
                         </div>
+                        <!-- ganti -->
+
                     </div>
                     <div class="ticket-info-right-time">
-                        <div>Show Time</div>
+                        <div class="label-datetime">Show Time</div>
                         <div class="line-datetime-container">
                             <img src="/public/icons/bookin-line-solid-2.svg" alt="Solid Line 2" />
                         </div>
                         <div class="ticket-info-right-time-container">
-                            <div class="button-text-plain">
+                            <!-- <div class="button-text-plain">
                                 <button>10:00</button>
                             </div>
                             <div class="button-text-plain">
@@ -62,35 +66,40 @@
                             </div>
                             <div class="button-text-plain">
                                 <button>19:00</button>
+                            </div> -->
+                            <div class="button-text-border">
+                                <button>13:45</Button>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div class="ticket-info-right-seating-container">
                     <img src="/public/icons/bookin-line-screen.svg" alt="Screen" />
-                    <div class="ticket-info-right-seating-screen">SCREEN</div>
+                    <div class="ticket-info-right-seating-screen">S C R E E N</div>
                     <div class="ticket-info-right-seating-available">Available Seats 96</div>
                     <img src="/public/icons/bookin-seats.svg" alt="Seats" />
                 </div>
                 <div class="ticket-info-right-purchase-container">
                     <div class="ticket-info-right-purchase-amount">
-                        <div>Enter the number of seats to be booked</div>
+                        <div class="label-ticket-info-right-purchase-amount">
+                            Enter the number of seats to be booked
+                        </div>
                         <div class="ticket-info-right-purchase-adjust">
-                            <div class="button-operator minus">
+                            <!-- <div class="button-operator minus">
                                 <button>-</button>
-                            </div>
+                            </div> -->
                             <div class="input-amount">
-                                <label for="amount" class="visually-hidden">Amount:</label>
-                                <input type="number" id="amount" name="amount" value="0" />
+                                <!-- <label for="amount" class="visually-hidden">Amount:</label> -->
+                                <input placeholder="Input Here" type="number" id="amount" name="amount" value="0" />
                             </div>
-                            <div class="button-operator plus">
+                            <!-- <div class="button-operator plus">
                                 <button>+</button> 
-                            </div>
+                            </div> -->
                         </div>
                     </div>
-                    <div class="button-next">
-                        <a href="/payment"><img src="/public/icons/bookin-button-next.svg" alt="Next Button" /></a>
-                    </div>
+                </div>
+                <div class="button-next">
+                    <a href="/payment"><img src="/public/icons/bookin-button-next.svg" alt="Next Button" /></a>
                 </div>
             </div>
         </div>
