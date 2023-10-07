3ab796b366a6a9cfa8c435b647135dfdacb632b2
Tansil011019 <13521054@std.stei.itb.ac.id>
2023-10-07 16:40:39 +0700

    refactor: remove var_dump

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -4,7 +4,6 @@ class Home extends Controller
 {
     public function index($queryParameters = [])
     {
-        // var_dump($_SESSION);
         $data['header'] = $this->model("HomeModel")->getHeader();
         $data['slidingCardMovies'] = $this->model("HomeModel")->getRandomData();
         $data['movies'] = $this->model("HomeModel")->getMovieByQuery($queryParameters);
