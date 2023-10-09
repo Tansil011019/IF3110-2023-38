a1037b3c8d2cac8dddf90daf32d3b0dbeba3b4b3
Tansil011019 <13521054@std.stei.itb.ac.id>
2023-10-09 13:44:38 +0700

    chore: customer list checkpoint

--- app/controllers/CustomerList.php
+++ app/controllers/CustomerList.php
@@ -2,13 +2,14 @@
 
 class CustomerList extends Controller
 {
-    public function index()
+    public function index($query = [])
     {
         $data['header'] = $this->model('CustomerListModel')->getHeader();
-        $data['customer'] = $this->model('CustomerListModel')->
+        $data['customer'] = $this->model('CustomerListModel')->getAllCustomer($query);
+        $data['customerCount'] = $this->model('CustomerListModel')->getAllCustomerCount();
 
         $this->view('templates/header', $data);
-        $this->view('customerList/index');
+        $this->view('customerList/index', $data);
         $this->view('templates/footer');
     }
 }
