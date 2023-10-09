10ba9a3cf5cb336b3e5627be8c494b057a3e8715
Tansil011019 <13521054@std.stei.itb.ac.id>
2023-10-09 11:21:59 +0700

    feat: add initial schedule page

--- app/controllers/Schedule.php
+++ app/controllers/Schedule.php
@@ -1,11 +1,13 @@
 <?php
 
 class Schedule extends Controller {
-    public function index() {
+    public function index($queryParameters = []) {
         $data['header'] = $this->model("ScheduleModel")->getHeader();
+        $data['movies'] = $this->model("HomeModel")->getMovieByQuery($queryParameters);
+        $data['movieCount'] = $this->model("HomeModel")->getCountDataByFilter($queryParameters)['count'];
 
         $this->view('templates/header', $data);
-        $this->view('schedule/index');
+        $this->view('schedule/index', $data);
         $this->view('templates/footer');
     }
 }
