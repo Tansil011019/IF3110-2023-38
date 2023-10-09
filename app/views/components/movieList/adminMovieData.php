d8eb00d7d0e317e24efec7944305386ece2c79c8
Agilham <13521118@std.stei.itb.ac.id>
2023-10-09 10:14:13 +0700

    fix: adjust href for BookinCard

--- app/views/components/movieList/adminMovieData.php
+++ app/views/components/movieList/adminMovieData.php
@@ -1,9 +1,7 @@
 <div class="container-grid-card-movies">
-
     <?php foreach ($data['movies'] as $datum) : ?>
         <?php require 'app/views/ui/bookinCard.php'; ?>
     <?php endforeach; ?>
-    
 </div>
 <?php 
     require_once 'app/views/ui/bookinPagination.php'
--- app/views/ui/bookinCard.php
+++ app/views/ui/bookinCard.php
@@ -1,5 +1,15 @@
 <div class="card-outer-container">
-    <a href="#">
+    <?php
+    $url = $_SERVER['REQUEST_URI'];
+    $parsedUrl = parse_url($url);
+    $path = $parsedUrl['path'];
+    if ($path == '/movie') {
+        $href = '/ticket/?movie_id=' . $datum['film_id'];
+    } else if ($path == '/movielist') {
+        $href = '/editmovie/?movie_id=' . $datum['film_id'];
+    }
+    ?>
+    <a href="<?php echo $href ?>">
         <div class="card-container">
                 <div class="card-image">
                     <img src="<?= $datum['poster_url']; ?>" alt="card image">
