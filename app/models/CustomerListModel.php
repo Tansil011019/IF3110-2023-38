53abf5e5cf41f24dec54f17827d1b11ab6546afc
Tansil011019 <13521054@std.stei.itb.ac.id>
2023-10-09 13:44:38 +0700

    chore: customer list checkpoint

--- app/models/CustomerListModel.php
+++ app/models/CustomerListModel.php
@@ -10,6 +10,11 @@ class CustomerListModel
     private $table = 'user';
     private $db;
 
+    private $defaultQuery = [
+        "page" => 1,
+        "pageSize" => 12,
+    ];
+
     public function __construct()
     {
         $this->db = new Database;
@@ -20,7 +25,24 @@ class CustomerListModel
         return $this->modelInfo;
     }
 
-    public function getAllCustomer() {
-        $finalQuery = 'SELECT * FROM "' . $this->table . '" WHERE deleted_at IS NOT NULL AND user_role=' . USER_ROLE['ADMIN'];
+    public function getAllCustomer($query = []) {
+        $page = isset($query['page']) ? max(1, intval($query['page'])) : $this->defaultQuery['page'];
+        $pageSize = isset($query['pageSize']) ? intval($query['pageSize']) : $this->defaultQuery['pageSize'];
+
+        $offset = ($page - 1) * $pageSize;
+
+        $finalQuery = 'SELECT * FROM "' . $this->table . '" WHERE deleted_at IS NULL AND user_role=' . USER_ROLE['CUSTOMER'] . ' LIMIT ' . $pageSize . ' OFFSET ' . $offset;
+
+        $this->db->query($finalQuery);
+
+        return $this->db->resultSet();
+    }
+
+    public function getAllCustomerCount() {
+        $finalQuery = 'SELECT COUNT(*) FROM "' . $this->table . '" WHERE deleted_at IS NULL AND user_role=' . USER_ROLE['CUSTOMER'];
+
+        $this->db->query($finalQuery);
+
+        return $this->db->single();
     }
 }
