8d9e62eec93980196e569c48868d76b3cc081937
Agilham <13521118@std.stei.itb.ac.id>
2023-10-09 14:45:46 +0700

    feat: add movie

--- app/models/AddMovieModel.php
+++ app/models/AddMovieModel.php
@@ -1,6 +1,10 @@
 <?php
 class AddMovieModel
 {
+    private $filmTable = 'film';
+    private $scheduleTable = 'schedule';
+    private $db;
+
     private $modelInfo = [
         "title" => "Add Movie",
         "currentRoute" => "/addMovie",
@@ -10,5 +14,25 @@ class AddMovieModel
     public function getHeader()
     {
         return $this->modelInfo;
+    }
+
+    public function __construct()
+    {
+        $this->db = new Database;
+    }
+
+    public function createMovie($title, $genre, $description, $age_restriction, $duration, $starting_date, $end_date, $trailer_url_youtube, $poster_url) {
+        $this->db->query('INSERT INTO "' . $this->filmTable . '" (title, genre, description, age_restriction, duration, starting_date, end_date, trailer_url_youtube, poster_url, created_by) VALUES (\'' . $title . '\', \'' . $genre . '\', \'' . $description . '\', \'' . $age_restriction . '\', \'' . $duration . '\', \'' . $starting_date . '\', \'' . $end_date . '\',  \'' . $trailer_url_youtube . '\' , \'' . $poster_url . '\' , \'' . $_SESSION['userId'] . '\')');
+        $this->db->execute();
+    }
+
+    public function updateMovieTrailer($trailer_url) {
+        $this->db->query('UPDATE "' . $this->filmTable . '" SET trailer_url = \'' . $trailer_url . '\', updated_by = \'' . $_SESSION['userId'] . '\' WHERE id = \'' . $_SESSION['movieId'] . '\'');
+        $this->db->execute();
+    }
+
+    public function updateMovieThumbnail($thumbnail_url) {
+        $this->db->query('UPDATE "' . $this->filmTable . '" SET thumbnail_url = \'' . $thumbnail_url . '\', updated_by = \'' . $_SESSION['userId'] . '\' WHERE id = \'' . $_SESSION['movieId'] . '\'');
+        $this->db->execute();
     }
 }
--- app/views/addMovie/index.php
+++ app/views/addMovie/index.php
@@ -1,198 +1,228 @@
+<?php
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $title = $_POST['movie-title'];
+        $genre = $_POST['genre-movie'];
+        $description = $_POST['movie-desc'];
+        $age_restriction = $_POST['movie-agelimit'];
+        $duration = $_POST['movie-duration'];
+        $starting_date = $_POST['movie-start-date'];
+        $end_date = $_POST['movie-end-date'];
+        $trailer_url_youtube = $_POST['url-movie'];
+
+        $file_name = $_FILES['poster-input']['name'];
+        $file_size = $_FILES['poster-input']['size'];
+        $file_type = $_FILES['poster-input']['type'];
+        $file_tmp = $_FILES['poster-input']['tmp_name'];
+        $destinationDir = '/var/www/html/public/images/';
+        move_uploaded_file($file_tmp, $destinationDir . $file_name);
+        $poster_url = '/public/images/' . $file_name;
+
+        try {
+            $this->model('AddMovieModel')->createMovie($title, $genre, $description, $age_restriction, $duration, $starting_date, $end_date, $trailer_url_youtube, $poster_url, $thumbnail_url);
+            $movieStatus = "Completed";
+        } catch (PDOException $error) {
+            $movieStatus = "Failed";
+        }
+    }
+?>
+
 <div class="main-container">
     <div class="head-container">
         Add Movie
     </div>
-    <div class="input-field-container">
-        <div class="left-side">
-            <div class="title-field">
-                <div class="addMovie-label">
-                    Movies Title
-                </div>
-                <?php
-                require_once 'app/views/libs/function/BookinInput.php';
-                $inputProps = [
-                    'name' => 'movie-title',
-                    'placeHolder' => "Input Title",
-                    'type' => 'text',
-                ];
-                $inputField = BookinInput($inputProps);
-                echo $inputField;
-                ?>
-            </div>
-            <div class="genre-field">
-                <div class="addMovie-label">
-                    Genre
-                </div>
-                <?php
-                require_once 'app/views/libs/function/BookinInput.php';
-                $inputProps = [
-                    'name' => 'genre-movie',
-                    'placeHolder' => "Input Genre",
-                    'type' => 'text',
-                ];
-                $inputField = BookinInput($inputProps);
-                echo $inputField;
-                ?>
-            </div>
-            <div class="url-field">
-                <div class="addMovie-label">
-                    URL Trailer
-                </div>
-                <?php
-                require_once 'app/views/libs/function/BookinInput.php';
-                $inputProps = [
-                    'name' => 'url-movie',
-                    'placeHolder' => "Input URL",
-                    'type' => 'url',
-                ];
-                $inputField = BookinInput($inputProps);
-                echo $inputField;
-                ?>
-            </div>
-            <div class="desc-fild">
-                <div class="addMovie-label">
-                    Description
-                </div>
-                <?php
-                require_once 'app/views/libs/function/BookinInput.php';
-                $inputProps = [
-                    'name' => 'movie-desc',
-                    'placeHolder' => "Text",
-                    'type' => 'text',
-                ];
-                $inputField = BookinInput($inputProps);
-                echo $inputField;
-                ?>
-            </div>
-            <div class="movie-start-time-button">
-                <div class="addMovie-label">
-                    Select a Start Date
-                </div>
-                <input type="date" id="start-date-input">
-                <span id="selected-start-date"></span>
-                <script src="public\js\add-movie.js"></script>
-            </div>
-        </div>
-        <div class="right-side">
-            <div class="duration-and-agelimit-field">
-                <div class="duration-field">
+    <form action="/addMovie" method="post" id="add-movie-form" enctype="multipart/form-data">
+        <div class="input-field-container">
+            <div class="left-side">
+                <div class="title-field">
                     <div class="addMovie-label">
-                        Duration
+                        Movies Title
                     </div>
                     <?php
                     require_once 'app/views/libs/function/BookinInput.php';
                     $inputProps = [
-                        'name' => 'movie-duration',
-                        'placeHolder' => "Input Duration",
-                        'type' => 'number',
-                        'innerStyles' => '-duration',
-                        'outerStyles' => '-duration'
+                        'name' => 'movie-title',
+                        'placeHolder' => "Input Title",
+                        'type' => 'text',
                     ];
                     $inputField = BookinInput($inputProps);
                     echo $inputField;
                     ?>
                 </div>
-                <div class="agelimit-field">
+                <div class="genre-field">
                     <div class="addMovie-label">
-                        Age Limit
+                        Genre
                     </div>
                     <?php
                     require_once 'app/views/libs/function/BookinInput.php';
                     $inputProps = [
-                        'name' => 'movie-agelimit',
-                        'placeHolder' => "Input Age Limit",
-                        'type' => 'number',
-                        'innerStyles' => '-agelimit',
-                        'outerStyles' => '-agelimit'
+                        'name' => 'genre-movie',
+                        'placeHolder' => "Input Genre",
+                        'type' => 'text',
                     ];
                     $inputField = BookinInput($inputProps);
                     echo $inputField;
                     ?>
                 </div>
-            </div>
-
-
-            <div class="fee-and-quota-field">
-                <div class="fee-field">
+                <div class="url-field">
                     <div class="addMovie-label">
-                        Fee
+                        URL Trailer
                     </div>
                     <?php
                     require_once 'app/views/libs/function/BookinInput.php';
                     $inputProps = [
-                        'name' => 'movie-fee',
-                        'placeHolder' => "Input Fee",
-                        'type' => 'number',
-                        'innerStyles' => '-fee',
-                        'outerStyles' => '-fee'
+                        'name' => 'url-movie',
+                        'placeHolder' => "Input URL",
+                        'type' => 'url',
                     ];
                     $inputField = BookinInput($inputProps);
                     echo $inputField;
                     ?>
                 </div>
-                <div class="quota-field">
+                <div class="desc-fild">
                     <div class="addMovie-label">
-                        Quota
+                        Description
                     </div>
                     <?php
                     require_once 'app/views/libs/function/BookinInput.php';
                     $inputProps = [
-                        'name' => 'movie-quota',
-                        'placeHolder' => "Input quota",
-                        'type' => 'number',
-                        'innerStyles' => '-quota',
-                        'outerStyles' => '-quota'
+                        'name' => 'movie-desc',
+                        'placeHolder' => "Text",
+                        'type' => 'text',
                     ];
                     $inputField = BookinInput($inputProps);
                     echo $inputField;
                     ?>
                 </div>
-            </div>
-
-
-            <div class="select-trailer-poster-button">
-                <div class="movie-trailer-button">
+                <div class="movie-start-time-button">
                     <div class="addMovie-label">
-                        Select Movie Trailer
+                        Select a Start Date
                     </div>
-                    <input type="file" id="file-trailer-input" style="display: none;">
-                    <button id="select-trailer-button" onclick="document.getElementById('file-trailer-input').click()">Select</button>
-                    <span id="file-name"></span>
+                    <input type="date" name="movie-start-date" id="start-date-input">
+                    <span id="selected-start-date"></span>
                     <script src="public\js\add-movie.js"></script>
                 </div>
-                <div class="movie-poster-button">
-                    <div class="addMovie-label">
-                        Select Movie Poster
+            </div>
+            <div class="right-side">
+                <div class="duration-and-agelimit-field">
+                    <div class="duration-field">
+                        <div class="addMovie-label">
+                            Duration
+                        </div>
+                        <?php
+                        require_once 'app/views/libs/function/BookinInput.php';
+                        $inputProps = [
+                            'name' => 'movie-duration',
+                            'placeHolder' => "Input Duration",
+                            'type' => 'number',
+                            'innerStyles' => '-duration',
+                            'outerStyles' => '-duration'
+                        ];
+                        $inputField = BookinInput($inputProps);
+                        echo $inputField;
+                        ?>
                     </div>
-                    <input type="file" id="file-poster-input" style="display: none;">
-                    <button id="select-poster-button" onclick="document.getElementById('file-poster-input').click()">Select</button>
+                    <div class="agelimit-field">
+                        <div class="addMovie-label">
+                            Age Limit
+                        </div>
+                        <?php
+                        require_once 'app/views/libs/function/BookinInput.php';
+                        $inputProps = [
+                            'name' => 'movie-agelimit',
+                            'placeHolder' => "Input Age Limit",
+                            'type' => 'number',
+                            'innerStyles' => '-agelimit',
+                            'outerStyles' => '-agelimit'
+                        ];
+                        $inputField = BookinInput($inputProps);
+                        echo $inputField;
+                        ?>
+                    </div>
+                </div>
+
+
+                <div class="fee-and-quota-field">
+                    <div class="fee-field">
+                        <div class="addMovie-label">
+                            Fee
+                        </div>
+                        <?php
+                        require_once 'app/views/libs/function/BookinInput.php';
+                        $inputProps = [
+                            'name' => 'movie-fee',
+                            'placeHolder' => "Input Fee",
+                            'type' => 'number',
+                            'innerStyles' => '-fee',
+                            'outerStyles' => '-fee'
+                        ];
+                        $inputField = BookinInput($inputProps);
+                        echo $inputField;
+                        ?>
+                    </div>
+                    <div class="quota-field">
+                        <div class="addMovie-label">
+                            Quota
+                        </div>
+                        <?php
+                        require_once 'app/views/libs/function/BookinInput.php';
+                        $inputProps = [
+                            'name' => 'movie-quota',
+                            'placeHolder' => "Input quota",
+                            'type' => 'number',
+                            'innerStyles' => '-quota',
+                            'outerStyles' => '-quota'
+                        ];
+                        $inputField = BookinInput($inputProps);
+                        echo $inputField;
+                        ?>
+                    </div>
+                </div>
+
+
+                <div class="select-trailer-poster-button">
+                    <div class="movie-trailer-button">
+                        <div class="addMovie-label">
+                            Select Movie Trailer
+                        </div>
+                        <input type="file" name="trailer-input" id="file-trailer-input" style="display: none;">
+                        <button type="button" id="select-trailer-button" onclick="document.getElementById('file-trailer-input').click()">Select</button>
+                        <span id="file-name"></span>
+                        <script src="public\js\add-movie.js"></script>
+                    </div>
+                    <div class="movie-poster-button">
+                        <div class="addMovie-label">
+                            Select Movie Poster
+                        </div>
+                        <input type="file" name="poster-input" id="file-poster-input" style="display: none;">
+                        <button type="button" id="select-poster-button" onclick="document.getElementById('file-poster-input').click()">Select</button>
+                        <span id="file-name"></span>
+                        <script src="public\js\add-movie.js"></script>;
+                    </div>
+                </div>
+                <div class="url-thumbnail">
+                    <div class="addMovie-label">
+                        Select Movie Thumbnail
+                    </div>
+                    <input type="file" name="thumbnail-input" id="file-thumbnail-input" style="display: none;">
+                    <button type="button" id="select-thumbnail-button" onclick="document.getElementById('file-thumbnail-input').click()">Select</button>
                     <span id="file-name"></span>
                     <script src="public\js\add-movie.js"></script>;
                 </div>
-            </div>
-            <div class="url-thumbnail">
-                <div class="addMovie-label">
-                    Select Movie Thumbnail
-                </div>
-                <input type="file" id="file-thumbnail-input" style="display: none;">
-                <button id="select-thumbnail-button" onclick="document.getElementById('file-thumbnail-input').click()">Select</button>
-                <span id="file-name"></span>
-                <script src="public\js\add-movie.js"></script>;
-            </div>
-            <div class="select-limit-time">
-                <div class="movie-end-time-button">
-                    <div class="addMovie-label">
-                        Select an End Date
+                <div class="select-limit-time">
+                    <div class="movie-end-time-button">
+                        <div class="addMovie-label">
+                            Select an End Date
+                        </div>
+                        <input type="date" name="movie-end-date" id="end-date-input">
+                        <span id="selected-end-date"></span>
+                        <script src="public\js\add-movie.js"></script>
                     </div>
-                    <input type="date" id="end-date-input">
-                    <span id="selected-end-date"></span>
+                </div>
+                <div class="button-addMovie">
+                    <button type="button" id="add-movie">Add</button>
                     <script src="public\js\add-movie.js"></script>
                 </div>
             </div>
-            <div class="button-addMovie">
-                <button id="add-movie">Add</button>
-                <script src="public\js\add-movie.js"></script>
-            </div>
         </div>
-    </div>
+    </form>
 </div>
